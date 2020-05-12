7a6b06f3a4c0e490d0bfdf64ae2fc2588565b13d
NaudR366 <57040315+NaudR366@users.noreply.github.com>
2020-05-12 14:48:47 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,66 +1,45 @@
-window.addEventListener("load", createLife);
-
-//get game element
 let game = document.getElementsByTagName("game")[0]
-game.addEventListener("click", clickHandler);
 
-//set amount of fish and bubbles
-let amount = 100;
-
-//create kill counter var
-let killCounter = document.createElement("h1")
-killCounter.innerHTML = "Kills: 0"
-game.appendChild(killCounter)
-
-function createLife() {
-    for (let i = 0; i < amount; i++) {
-        let fishInfo = {
-            x: getRandomInt(screen.width - 100),
-            y: getRandomInt(screen.height -250),
-            color: getRandomInt(361)
-        }
-
-        //info about location of bubble
-        bubbleLocation = {
-            x: getRandomInt(screen.width - 50),
-            y: getRandomInt(screen.height -250),
-        }
-
-        // vis element
-
-        let fish = document.createElement("fish")
-        game.appendChild(fish)
-        fish.style.transform = `translate(${fishInfo.x}px, ${fishInfo.y}px)`
-        fish.style.filter = `hue-rotate(${fishInfo.color}deg)`
-        fish.setAttribute("id", i)
-        fish.classList.add("fish")
-
-        // bubble element
-
-        let bubble = document.createElement("bubble")
-        game.appendChild(bubble)
-        bubble.style.transform = `translate(${bubbleLocation.x}px,${bubbleLocation.y}px)`
-        bubble.classList.add("bubble")
-    }
+for (let i = 0; i < 100; i++) {
+    createFish()
+    createBubble()    
 }
 
-function clickHandler() {
-    let target = event.target
-    console.log(target)
 
-    //check if target is a fish
-    if(target.classList.contains("fish")) {
-        target.classList.add("dead")
-    }
+function createFish() {
+    //
+    // vis element
+    //
+    let fish = document.createElement("fish")
+    fish.addEventListener("click", onFishClick)
+    
+    game.appendChild(fish)
 
-    //check if target is a bubble
-    if(target.classList.contains("bubble")) {
-        target.remove()
-    }
+    x = Math.random() * (window.innerWidth - fish.clientWidth)
+    y = Math.random() * (window.innerHeight - fish.clientHeight)
+    color = Math.random() * 360
 
+    fish.style.transform = `translate(${x}px, ${y}px)`
+    fish.style.filter = `hue-rotate(${color}deg)`
 }
 
-//random number
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+function createBubble() {
+    //
+    // bubble element
+    //
+    let bubble = document.createElement("bubble")
+    bubble.addEventListener("click", onBubbleClick)
+    game.appendChild(bubble)
+
+    x = Math.random() * (window.innerWidth - bubble.clientWidth)
+    y = window.innerHeight - bubble.clientHeight
+    bubble.style.transform = `translate(${x}px, ${y}px)`
+}
+
+function onFishClick(event) {
+    event.target.classList.add("dead")
+}
+
+function onBubbleClick() {
+    event.target.remove()
 }
